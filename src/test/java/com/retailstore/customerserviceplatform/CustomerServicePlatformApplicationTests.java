ec0b99ef06e2c1ad6a40b8c6d0e46aa81e26f8b3
mojojojo1234 <panjwanichirag0316@gmail.com>
2026-03-22 18:24:12 +0530

    Used Stanford CoreNLP to perform sentiment analysis

---
+++ src/test/java/com/retailstore/customerserviceplatform/CustomerServicePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailstore.customerserviceplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServicePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
